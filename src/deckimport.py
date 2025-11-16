8b2a1aa93076d9cdf8ae072859a56e3550df3ef5
bored_dam <damianotrovato122@gmail.com>
2025-11-16 01:59:11 +0100

    feat(deckimport.py): added function to download decks json files from github.

--- src/deckimport.py
+++ src/deckimport.py
@@ -8,4 +8,20 @@ def _convert_github_url_to_raw(url:str) -> str:
     return url
 
 
-
+def get_deck_from_link(url:str) -> list:
+    
+    url = _convert_github_url_to_raw(url)
+    
+    if not url.endswith(".json"):
+        return []
+    
+    response = requests.get(url)    
+    if response.status_code != 200:
+        return []
+    
+    try:
+        deck = response.json()
+        return deck
+    
+    except requests.exceptions.JSONDecodeError:
+        return []
