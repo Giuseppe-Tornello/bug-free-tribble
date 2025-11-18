7ad39d866ee0128d1085ea125c23cab50be7024b
Damiano Trovato <damianotrovato122@gmail.com>
2025-11-18 20:14:43 +0100

    feat(deckimport.py): created functions related to deck fetching from github (#14)
    * feat(deckimport.py): added function that converts regular github links to raw.githubusercontent links
    
    * feat(deckimport.py): added function to download decks json files from github.
    
    * style(deckimport.py): improved readability
    
    * style(deckimport.py): used json constant
    
    ---------
    
    Co-authored-by: Giuseppe Tornello <giuseppe.tornello05@protonmail.com>

---
+++ src/deckimport.py
@@ -0,0 +1,29 @@
+import requests
+from .data.constants import DECKS_EXTENSION
+
+
+def _convert_github_url_to_raw(url: str) -> str:
+    if url.startswith("https://github.com/"):
+        url = url.replace("github.com", "raw.githubusercontent.com", 1)
+        url = url.replace("blob", "refs/heads", 1)
+    return url
+
+
+def get_deck_from_link(url: str) -> list:
+    
+    url = _convert_github_url_to_raw(url)
+    
+    if not url.endswith(DECKS_EXTENSION):
+        return []
+    
+    response = requests.get(url)    
+    if response.status_code != 200:
+        return []
+    
+    try:
+        deck = response.json()
+        return deck
+    
+    except requests.exceptions.JSONDecodeError:
+        return []
+
