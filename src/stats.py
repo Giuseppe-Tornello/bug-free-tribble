cfbcbe69a0a72b80787c108daebb6b567c86ad48
IsmaeleGaldino <iglike20@gmail.com>
2025-12-05 19:01:01 +0100

    style: spacing

--- src/stats.py
+++ src/stats.py
@@ -3,5 +3,5 @@ from .data.constants import JSON_ENCODING , USER_PATH , STATS_EXTENSION
 from .cardcreation import _deck_name_to_path
 
 def stats_name(name: str) -> str:
-    return USER_PATH+"stats_"+name+STATS_EXTENSION
+    return USER_PATH + "stats_" + name + STATS_EXTENSION
 
